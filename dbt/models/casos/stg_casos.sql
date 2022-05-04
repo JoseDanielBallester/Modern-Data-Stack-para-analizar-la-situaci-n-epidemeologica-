f000532aa0b137ed536f3075e92519396396342f
josedanielballester@gmail.com <josedanielballester@gmail.com>
2022-05-04 18:40:11 +0000

    modelo casos

---
+++ dbt/models/casos/stg_casos.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        unique_key="ID_CASOS"
+    )
+}}
+
+select
+    PROVINCIA_ISO AS ID_PROVINCIA,
+    SEXO,
+    GRUPO_EDAD,
+    FECHA AS ID_FECHA,
+    NUM_CASOS,
+    NUM_HOSP,
+    NUM_UCI,
+    NUM_DEF,
+    ID_PROVINCIA||SEXO||GRUPO_EDAD||ID_FECHA AS ID_CASOS
+from {{ source('casos', 'cases') }} 
