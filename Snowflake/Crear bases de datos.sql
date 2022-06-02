63b4475c0f753b88d6465f98b275175cf9d4c749
JoseDanielBallester <josedanielballester@gmail.com>
2022-06-02 23:59:26 +0200

    Update Crear bases de datos.sql

--- Snowflake/Crear bases de datos.sql
+++ Snowflake/Crear bases de datos.sql
@@ -21,6 +21,30 @@ create or replace table cases
 create or replace FILE FORMAT "RAW"."COVID".CSV TYPE = 'CSV' COMPRESSION = 'AUTO' FIELD_DELIMITER = ','
 RECORD_DELIMITER = '\n' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = 'NONE' TRIM_SPACE = FALSE
 ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE ESCAPE = 'NONE' ESCAPE_UNENCLOSED_FIELD = '\134'
+DATE_FORMAT = 'AUTO' TIMESTAMP_FORMAT = 'AUTO' NULL_IF = ('\\N');
+create or replace warehouse transforming;
+
+create or replace database raw;
+
+create or replace database analytics;
+
+create or replace schema raw.covid;
+
+create or replace table cases
+(
+    provincia_iso varchar,
+    sexo varchar,
+    grupo_edad varchar,
+    fecha date,
+    num_casos integer,
+    num_hosp integer,
+    num_uci integer,
+    num_def integer
+);
+
+create or replace FILE FORMAT "RAW"."COVID".CSV TYPE = 'CSV' COMPRESSION = 'AUTO' FIELD_DELIMITER = ','
+RECORD_DELIMITER = '\n' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = 'NONE' TRIM_SPACE = FALSE
+ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE ESCAPE = 'NONE' ESCAPE_UNENCLOSED_FIELD = '\134'
 DATE_FORMAT = 'AUTO' TIMESTAMP_FORMAT = 'AUTO' NULL_IF = ('\\N') ENCODING = 'ISO-8859-1';
 
 create or replace table Hospitals
@@ -68,4 +92,26 @@ create or replace table Residences
 create or replace FILE FORMAT "RAW"."COVID".CVSNODATE COMPRESSION = 'AUTO' FIELD_DELIMITER = ';'
 RECORD_DELIMITER = '\n' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = 'NONE' TRIM_SPACE = FALSE
 ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE ESCAPE = 'NONE' ESCAPE_UNENCLOSED_FIELD = '\134'
-TIMESTAMP_FORMAT = 'AUTO' DATE_FORMAT = 'AUTO' NULL_IF = ('\\N') ENCODING = 'ISO-8859-1' ;
+TIMESTAMP_FORMAT = 'AUTO' DATE_FORMAT = 'AUTO' NULL_IF = ('\\N') ENCODING = 'ISO-8859-1';
+
+create or replace table Population
+(
+    Provincias varchar,
+    "Edad (grupos quinquenales)" varchar,
+    "Españoles/Extranjeros" varchar,
+    Sexo varchar,
+    "Año" integer,
+    Total varchar
+    
+);
+create or replace table Actual_Population
+(
+
+    Sexo varchar,
+    "Total Nacional" varchar,
+    Comunidades varchar,
+    Provincias varchar,
+    "Edad (hasta 100 y más)" varchar,
+    Total varchar
+    
+);
