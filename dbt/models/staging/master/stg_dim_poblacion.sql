c253d28c0b4be247408a2a718535d610ced07912
josedanielballester@gmail.com <josedanielballester@gmail.com>
2022-06-03 21:17:49 +0000

    stg_poblacion_actual

---
+++ dbt/models/staging/master/stg_dim_poblacion.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        unique_key="ID_POBLACION"
+    )
+}}
+
+select
+    Provincias AS PROVINCIAS,
+    "Edad (grupos quinquenales)" AS EDAD,
+    "Españoles/Extranjeros" AS PROCEDENCIA,
+    Sexo AS SEXO,
+    "Año" AS FECHA,
+    Total AS TOTAL,
+    FECHA||PROVINCIAS||EDAD||PROCEDENCIA||SEXO||FECHA AS ID_POBLACION
+from {{ source('master', 'population') }}
