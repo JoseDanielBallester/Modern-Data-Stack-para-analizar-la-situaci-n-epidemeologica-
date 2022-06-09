9428b8b681c0067136cc63a7f3a7159268d7e0f0
josedanielballester@gmail.com <josedanielballester@gmail.com>
2022-06-09 22:39:26 +0000

    actualizada dimension provincias

--- dbt/models/temportal/master/tmp_dim_provincias.sql
+++ dbt/models/temportal/master/tmp_dim_provincias.sql
@@ -18,6 +18,7 @@ select
     CODIGO,
     SPLIT_PART(SPLIT_PART(comunidades_aux.PROVINCIA,'/',1),',',1) AS PROVINCIA,
     comunidades_aux.CCAA_ID AS ID_COMUNIDAD,
-    comunidades_aux.CCAA AS CCAA
+    comunidades_aux.CCAA AS CCAA,
+    CAPITAL_CCAA_ID
 from {{ ref('stg_dim_provincias')}}
 left join comunidades_aux using(ID)
